948f716da038b9025a9ea95eea0aeecf25489122
may-leth <mayleth9609@gmail.com>
2025-06-10 13:23:11 +0200

    feat(services):add constructor and findAll and add methods

--- src/main/java/com/example/GiggleBot/services/FunnyPhraseService.java
+++ src/main/java/com/example/GiggleBot/services/FunnyPhraseService.java
@@ -1,10 +1,24 @@
 package com.example.GiggleBot.services;
 
-
+import com.example.GiggleBot.models.FunnyPhrase;
+import com.example.GiggleBot.repositories.FunnyPhraseRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 
 @Service
 public class FunnyPhraseService {
+    private final FunnyPhraseRepository funnyPhraseRepository;
 
+    public FunnyPhraseService(FunnyPhraseRepository funnyPhraseRepository) {
+        this.funnyPhraseRepository = funnyPhraseRepository;
+    }
+
+    public List<FunnyPhrase> getAllFunnyPhrases() {
+        return funnyPhraseRepository.findAll();
+    }
+
+    public FunnyPhrase addFunnyPhrase(FunnyPhrase funnyPhrase){
+        return funnyPhraseRepository.save(funnyPhrase);
+    }
 }
